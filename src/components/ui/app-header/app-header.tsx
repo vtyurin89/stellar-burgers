7f9d8fd275957105b76046010abe119ac7622e57
Vladimir <vlad89main@gmail.com>
2026-04-30 15:46:28 +0300

    feat: header active links

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -7,39 +7,51 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
-  <header className={styles.header}>
-    <nav className={`${styles.menu} p-4`}>
-      <div className={styles.menu_part_left}>
-        <>
-          <Link to='/' className={styles.link}>
+export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
+  const { pathname } = useLocation();
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.menu} p-4`}>
+        <div className={styles.menu_part_left}>
+          <Link
+            to='/'
+            className={`${styles.link} ${
+              pathname === '/' ? styles.link_active : ''
+            }`}
+          >
             <BurgerIcon type={'primary'} />
             <p className='text text_type_main-default ml-2 mr-10'>
               Конструктор
             </p>
           </Link>
-        </>
-        <>
-          <Link to='/feed' className={styles.link}>
+          <Link
+            to='/feed'
+            className={`${styles.link} ${
+              pathname.startsWith('/feed') ? styles.link_active : ''
+            }`}
+          >
             <ListIcon type={'primary'} />
             <p className='text text_type_main-default ml-2'>Лента заказов</p>
           </Link>
-        </>
-      </div>
-      <div className={styles.logo}>
-        <Logo className='' />
-      </div>
-      <Link
-        to='/profile'
-        className={`${styles.link} ${styles.link_position_last}`}
-      >
-        <ProfileIcon type={'primary'} />
-        <p className='text text_type_main-default ml-2'>
-          {userName || 'Личный кабинет'}
-        </p>
-      </Link>
-    </nav>
-  </header>
-);
+        </div>
+        <div className={styles.logo}>
+          <Logo className='' />
+        </div>
+        <Link
+          to='/profile'
+          className={`${styles.link} ${styles.link_position_last} ${
+            pathname.startsWith('/profile') ? styles.link_active : ''
+          }`}
+        >
+          <ProfileIcon type={'primary'} />
+          <p className='text text_type_main-default ml-2'>
+            {userName || 'Личный кабинет'}
+          </p>
+        </Link>
+      </nav>
+    </header>
+  );
+};
