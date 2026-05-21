e01218e6459c32fc5cf7ff5aa4cca07a2d399e75
Vladimir <vlad89main@gmail.com>
2026-05-21 14:26:05 +0300

    feat: har file

--- tests/constructor.pl.tsx
+++ tests/constructor.pl.tsx
@@ -1,14 +1,22 @@
+import path from 'path';
 import { test, expect } from '@playwright/test';
 
-// E2E-тесты конструктора бургера
+const harPath = path.join(__dirname, '../e2e/hars/ingredients.har');
 
+// Перехват запроса ингредиентов
 test('должен записать HAR-файл', async ({ page }) => {
-  await page.routeFromHAR('./e2e/hars/users.har', {
-    url: '**/users',
-    update: true,
+  await page.routeFromHAR(harPath, {
+    url: '**/ingredients',
+    update: false
   });
 
-  await page.goto('/login');
-  
-  await expect(page.getByRole('button', { name: /войти/i })).toBeVisible();
-}); 
+  await page.goto('/');
+
+  await expect(
+    page.getByRole('heading', { name: 'Соберите бургер' })
+  ).toBeVisible();
+  await expect(page.getByText('Краторная булка N-200i')).toBeVisible();
+  await expect(
+    page.getByText('Биокотлета из марсианской Магнолии')
+  ).toBeVisible();
+});
